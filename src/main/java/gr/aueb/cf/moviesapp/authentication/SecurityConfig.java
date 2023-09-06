f369c661db6d2427e389273e0bd37475ef8b9d2d
fbozas <fbozas@hotmail.com>
2023-09-06 20:38:21 +0300

    Added filterChain & webSecurityCustomizer methods

--- src/main/java/gr/aueb/cf/moviesapp/authentication/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/moviesapp/authentication/SecurityConfig.java
@@ -3,11 +3,18 @@ package gr.aueb.cf.moviesapp.authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +24,27 @@ public class SecurityConfig {
     @Autowired
     public SecurityConfig(CustomAuthenticationProvider authProvider) {
         this.authProvider = authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/search").permitAll()
+                .and().httpBasic()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web ->  web.ignoring().antMatchers("/styles/**", "/img/**", "/js/**"));
     }
 
     public void configure(AuthenticationManagerBuilder auth){
