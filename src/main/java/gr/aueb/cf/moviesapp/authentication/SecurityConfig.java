4f59d04c755856e7f367a6c305db265ef9704078
fbozas <fbozas@hotmail.com>
2023-09-12 16:56:16 +0300

    Some additions on SecurityConfig

--- src/main/java/gr/aueb/cf/moviesapp/authentication/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/moviesapp/authentication/SecurityConfig.java
@@ -29,6 +29,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable()
+                .authorizeRequests().antMatchers("/login").permitAll()
+                .and()
                 .authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
                 .and()
                 .authorizeRequests().antMatchers("/register").permitAll()
