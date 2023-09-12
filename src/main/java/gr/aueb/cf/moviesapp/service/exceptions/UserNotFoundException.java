826cbee61c084635860a66f7ec38769d4a3556a7
fbozas <fbozas@hotmail.com>
2023-09-12 17:42:50 +0300

    Added UserNotFoundException class

---
+++ src/main/java/gr/aueb/cf/moviesapp/service/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.moviesapp.service.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+}
